a4966eaff8b22bf1de79c3c849767516c5260e8b
Richard Alvin <Richard Alvin@DESKTOP-4PVMJKS>
2023-12-13 10:45:00 +0700

    build user and authentication

--- src/api/album/routes.js
+++ src/api/album/routes.js
@@ -3,6 +3,9 @@ const routes = (handler) => [
         method: 'POST',
         path: '/albums',
         handler: handler.postAlbumHandler,
+        options: {
+            auth: 'openmusicapi_jwt',
+        },
     },
     {
         method: 'GET',
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const ClientError = require('./exceptions/ClientError');
 
 //  album
@@ -13,6 +14,17 @@ const song = require('./api/song');
 const SongService = require('./services/postgres/SongService');
 const SongValidator = require('./validator/song');
 
+// user
+const user = require('./api/user');
+const UserService = require('./services/postgres/UserService');
+const UserValidator = require('./validator/user');
+
+// authentications
+const authentication = require('./api/authentication');
+const AuthenticationService = require('./services/postgres/AuthenticationService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationValidator = require('./validator/authentication');
+
 const init = async () => {
     const server = Hapi.server({
         port: process.env.PORT,
@@ -24,21 +36,62 @@ const init = async () => {
         },
     });
 
-    await server.register({
-        plugin: album,
-        options: {
-            service: new AlbumService(),
-            validator: AlbumValidator,
+    // registrasi plugin eksternal
+    await server.register([
+        {
+            plugin: Jwt,
         },
+    ]);
+
+    // mendefinisikan strategy autentikasi jwt
+    server.auth.strategy('openmusicapi_jwt', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
     });
 
-    await server.register({
-        plugin: song,
-        options: {
-            service: new SongService(),
-            validator: SongValidator,
+    await server.register([
+        {
+            plugin: album,
+            options: {
+                service: new AlbumService(),
+                validator: AlbumValidator,
+            },
         },
-    });
+        {
+            plugin: song,
+            options: {
+                service: new SongService(),
+                validator: SongValidator,
+            },
+        },
+        {
+            plugin: user,
+            options: {
+                service: new UserService(),
+                validator: UserValidator,
+            },
+        },
+        {
+            plugin: authentication,
+            options: {
+                authenticationService: new AuthenticationService(),
+                userService: new UserService(),
+                tokenManager: TokenManager,
+                validator: AuthenticationValidator,
+            },
+        },
+    ]);
 
     server.ext('onPreResponse', (request, h) => {
         const { response } = request;
